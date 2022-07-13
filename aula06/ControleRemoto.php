0e12fc71faf8f3e1fd2604f594d0c9437591f5b6
Elias G. Pacífico <eliaspacifico6@gmail.com>
2022-07-13 13:52:55 -0300

    Aula 06

---
+++ aula06/ControleRemoto.php
@@ -0,0 +1,89 @@
+<?php
+require_once 'Controlador.php';
+class ControleRemoto implements Controlador {
+    // Atributos
+    private $volume;
+    private $ligado;
+    private $tocando;
+    // Métodos especiais
+    function __construct(){
+        $this->volume = 50;
+        $this->ligado = false;
+        $this->tocando = false;
+    }
+
+    function getVolume(){
+        return $this->volume;
+    }
+    function getLigado(){
+        return $this->ligado;
+    }
+    function getTocando(){
+        return $this->tocando;
+    }
+
+    function setVolume($volume){
+        $this->volume = $volume;
+    }
+    function setLigado($ligado){
+        $this->ligado = $ligado;
+    }
+    function setTocando($tocando){
+        $this->tocando = $tocando;
+    }
+
+    public function ligar(){
+        $this->setLigado(true);
+    }
+    public function desligar(){
+        $this->setLigado(false);
+
+    }
+    public function abrirMenu(){
+        echo "<br> Está Ligador?: ".  ($this->getLigado()?"SIM":"NÃO");
+        echo "<br> Está Tocando?: ". ($this->getTocando()?"SIM":"NÃO");
+        echo "<br> Volume: ". $this->getVolume();
+
+        for ($i=0; $i < $this->getVolume(); $i+=5) {  
+            echo "|";
+        }
+        echo "<br>";
+    }
+    public function fecharMenu(){
+        echo "Fechando Menu...";
+    }
+    public function maisVolume(){
+        if ($this->getLigado()){
+            $this->setVolume($this->getVolume() + 5);
+        }
+    }
+    public function menosVolume(){
+        if ($this->getLigado()){
+            $this->setVolume($this->getVolume() - 5);
+        }
+
+    }
+    public function ligarMudo(){
+        if ($this->getLigado() && $this->getVolume() > 0 ){
+            $this->setVolume(0);
+        }
+    }
+    public function desligarMudo(){
+        if ($this->getLigado() && $this->getVolume() == 0 ){
+            $this->setVolume(50);
+        }
+
+    }
+    public function paly(){        
+        if ($this->getLigado() && !($this->getTocando()) ){
+            $this->setTocando(true);
+        }
+    }
+    public function pause(){     
+        if ($this->getLigado() && ($this->getTocando()) ){
+            $this->setTocando(false);
+        }
+
+    }
+
+}

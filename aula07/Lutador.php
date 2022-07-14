578a2c866c7b36d3e7beebfa775d3f4f0cca2dd9
Elias G. Pacífico <eliaspacifico6@gmail.com>
2022-07-14 14:55:00 -0300

    aula 8

--- aula07/Lutador.php
+++ aula07/Lutador.php
@@ -14,10 +14,20 @@
 
         // Metodos Internos
         function apresesentar() {
-
+            echo "<p>----------------------------------------</p>";
+            echo "<p>Chegou a Hora!!! <br>";
+            echo "O lotador {$this->getNome()},de {$this->getIdade()} anos, {$this->getnacionalidade() } <br>";
+            echo "Pesando {$this->getPeso()} kilos, tem {$this->getVitorias()} Vitórias, {$this->getDerrotas()} Derrotas e  {$this->getEmpates()} Empates 
+            ";
+            echo "</p>";
         } 
         function status() {
-
+            echo "<p>----------------------------------------</p>";
+            echo "<p> {$this->getNome()} {$this->getIdade()} anos, {$this->getnacionalidade() } <br>";
+            echo "Peso: {$this->getPeso()} kilos, <br>";
+            echo "Vitórias: {$this->getVitorias()}<br>";
+            echo "Derrotas: {$this->getDerrotas()} <br>";
+            echo "Empates: {$this->getEmpates()} </p>";
         } 
         function ganharLuta() {
             $this->setVitorias($this->getVitorias() + 1);
@@ -30,6 +40,20 @@
             $this->setEmpates($this->getEmpates() + 1);
         } 
 
+        public function novoPeso($peso){
+            if ($peso > $this->getPeso()) {
+                echo "Lutador engordou ".  $peso - $this->getPeso() . " Kilos<br>";
+            } else {
+                
+                echo "Lutador emagreceu ".  $this->getPeso() - $peso . " Kilos<br>";
+            }
+
+            $this->setPeso($peso);
+
+        echo "Sua categora é :{$this->getCategoria()}<br>";
+        }
+
+
         // Metodos Especiais
         
         function __construct ($no,$na,$id,$al,$pe,$vi,$de,$em)  {
@@ -37,7 +61,7 @@
             $this-> nacionalidade = $na ;
             $this-> idade = $id ;
             $this-> altura = $al ;
-            $this-> peso = $pe ;
+            $this->setPeso($pe);
             $this-> vitorias = $vi ;
             $this-> derrotas = $de ;
             $this-> empates  = $em ;
@@ -66,7 +90,7 @@
             return $this->vitorias;
         }
         private function getDerrotas(){
-            return $this->derrota;
+            return $this->derrotas;
         }
         private function getEmpates(){
             return $this->empates;
@@ -88,16 +112,18 @@
         }
         private function setPeso($pe){
             $this->peso = $pe ;
-            $this-> setCategoria();
+            $this->setCategoria();
         }
+
         private function setCategoria(){
-           if  ($this->getPeso() < 52.2 ){
+                      
+           if  ($this->peso < 52.2 ){
                 $this->categoria = 'Invalido';
-           }elseif ($this->getPeso() < 70.3 ) {            
+           }elseif ($this->peso <= 70.3 ) {            
                 $this->categoria = 'Leve';
-           }elseif ($this->getPeso() < 83.9 ) {            
+           }elseif ($this->peso <= 83.9 ) {            
                 $this->categoria = 'Médio';
-           }elseif ($this->getPeso() < 12.2 ) {            
+           }elseif ($this->peso <= 120.2 ) {            
                 $this->categoria = 'Pesado';
            } else {
                 $this->categoria = 'Invalido';
--- aula07/index.php
+++ aula07/index.php
@@ -9,7 +9,24 @@
 <body>
     <pre>
         <?php
-        
+            require_once './Lutador.php';
+
+            $lutador[0] = new Lutador( "Pretty Boy", "Frances", 30, 1.75, 90.9, 11 ,2 ,1);
+            $lutador[1] = new Lutador( "Putscript", "Brasileiros", 29, 1.68, 57.8, 14, 2 ,3 );
+            $lutador[2] = new Lutador( "SnapShadow", "Americano", 356, 165, 80.9, 12, 2, 1);
+            $lutador[3] = new Lutador( "Dead Code", "Australiano", 28, 1.93, 81.6, 13, 0 ,2);
+            $lutador[4] = new Lutador( "UFO Cobol", "Brasileiros", 37, 1.70, 119.3, 5, 4, 3);
+            $lutador[5] = new Lutador( "Nerdaart", "Americano", 30, 1.81, 105.7, 12, 2, 4 );
+
+           
+
+            $lutador[1]->apresesentar();
+            $lutador[1]->status();
+            $lutador[3]->status();
+            $lutador[5]->status();
+            $lutador[2]->status();
+
+            print_r( $lutador);
         
         ?>
     </pre>
